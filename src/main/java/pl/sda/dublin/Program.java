388cbef49f581e845111232f89a83af891089666
Piotr Siwinski <siwinski.piotr@outlook.com>
2019-05-11 14:46:50 +0100

    Add quicksort algorithm

---
+++ src/main/java/pl/sda/dublin/Program.java
@@ -0,0 +1,14 @@
+package pl.sda.dublin;
+
+public class Program {
+    public static void main(String[] args) {
+
+        // pobierz od uzytkownika rozmiar tablicy
+
+        // utworz tablice i wypelnij ja liczbami pseudolosowymi
+
+        // utworz klase Stopwatch z zadanymi metodami
+
+        // posortuj tablice sortowaniem babelkowym
+    }
+}
---
+++ src/main/java/pl/sda/dublin/sorting/QuickSort.java
@@ -0,0 +1,61 @@
+package pl.sda.dublin.sorting;
+
+public class QuickSort {
+
+    // 1 - static
+    private static int[] numbers;
+
+    // 2 - static
+    public static void sort(int[] values) {
+        // sprawdzenie czy tablica ma wartosci
+        if (values == null || values.length == 0) {
+            System.out.println("pusta tablica");
+            return;
+        }
+        // 3 - bez this
+        numbers = values;
+        quicksort(0, numbers.length - 1);
+    }
+
+    // 4 - static
+    private static void quicksort(int low, int high) {
+        int i = low;
+        int j = high;
+
+        // wybierz pivot i midIndex
+        int midIndex = (low + high) / 2;
+        int pivot = numbers[midIndex];
+
+        // wykonuj tak długo, az indexy i oraz j sie spotkają
+        while (i <= j) {
+            // szukamy elementu wiekszego od pivota z lewej tablicy (wskaźnik i)
+            while (numbers[i] < pivot) {
+                i++;
+            }
+            // szukamy elementu mniejszego od pivota w prawej tablicy
+            // wykonuj tak dlugo az znajdziesz element mniejszy od pivota w prawej tablicy
+            while (numbers[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                // zamien miejscami i oraz j
+                int helper = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = helper;
+                i++;
+                j--;
+            }
+        }
+
+        // warunek zakończenia rekurencji
+        // jezeli zostało cos do sortowania
+        if (low < j) {
+            quicksort(low, j);
+        }
+        if (i < high) {
+            quicksort(i, high);
+        }
+
+
+    }
+}
